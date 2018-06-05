3bd3886192bcc24c60f97f30767e8dc40aebe5ff
Steinitz Emmanuel <manu@mbp-de-steinitz.lan>
2018-06-05 14:56:34 +0200

    Procfile & changes

---
+++ src/extractors/city-extractor.js
@@ -0,0 +1,12 @@
+
+
+const { WsExtractor } = require('botfuel-dialog');
+
+class CityExtractor extends WsExtractor {}
+
+CityExtractor.params = {
+  dimensions: ['city'],
+};
+
+module.exports = CityExtractor;
+
